c1c9d4f242796c37ecdf62e719905e41ad530762
Renato Santos <renato_jfs@hotmail.com>
2021-06-11 16:12:13 -0300

    chore: add demo07

---
+++ capitulo01/demo07-listas.js
@@ -0,0 +1,71 @@
+const minhaLista = []
+const minhaListaDeTarefas = [
+    'Mandar email',
+    'Colocar comida para o dog',
+    'Limpar o quarto'
+]
+
+// console.log(minhaListaDeTarefas[0])
+// console.log(minhaListaDeTarefas[1])
+// console.log(minhaListaDeTarefas[4])
+
+// quantidade de itens no array
+// console.log(minhaListaDeTarefas.length)
+
+// adicionar item no final do array
+minhaListaDeTarefas.push('Formatar computador')
+// console.log(minhaListaDeTarefas)
+
+// remover ultimo item do array
+// const ultimo = minhaListaDeTarefas.pop()
+// console.log(ultimo, minhaListaDeTarefas)
+
+// remover primeiro item do array
+// const primeiro = minhaListaDeTarefas.shift()
+// console.log(primeiro, minhaListaDeTarefas)
+
+// remover item especifico a partir de um indice
+// console.log(minhaListaDeTarefas[2])
+// qual item de inicio
+// quantos remover
+// minhaListaDeTarefas.splice(2,1)
+// console.log(minhaListaDeTarefas)
+
+const itens = [
+    1, 'Computador', 0.22
+]
+
+// verificar o tipo do array
+// console.log(typeof(itens))
+
+// verificar se é array da forma correta
+// console.log(Array.isArray(itens))
+
+// ordenar um array
+// const numeros = [3,1,2,0]
+// const letras = ['c','a','x','d']
+// console.log(numeros.sort())
+// console.log(letras.sort())
+
+// juntar dois arrays
+// const novo = itens.concat([1,2,3])
+// console.log(novo)
+
+// juntar arrays em uma string
+// console.log(itens.join(', '))
+
+// verificar indice do item
+// const index = itens.indexOf('Computador')
+// console.log(itens)
+// console.log(itens[index]) //PARA RETORNAR ITEN ESPECIFICO
+
+/**
+ * Exercicios de array
+ */
+// const lis = ['a', 'b', 'c']
+// console.log(lis[0])
+
+// const list = [1,2,3,4]
+// const item = 1
+// const it = list.indexOf(item)
+// console.log(it)
