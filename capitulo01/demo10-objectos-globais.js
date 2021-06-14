c6c577f4812490d916294c062b0d394a0d05ac7b
Renato Santos <renato_jfs@hotmail.com>
2021-06-14 15:44:31 -0300

    chore: add demo10

---
+++ capitulo01/demo10-objectos-globais.js
@@ -0,0 +1,7 @@
+console.log('keys', Object.keys({nome: 'Renato'}))
+console.log('values', Object.values({nome: 'Renato'}))
+console.log('now', Date.now())
+console.log('random', Math.random())
+
+// global
+console.log('global', global)
