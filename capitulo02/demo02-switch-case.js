ff07ec899cbcc001e89d86c3458f04232704b7cd
Renato Santos <renato_jfs@hotmail.com>
2021-06-17 00:32:05 -0300

    chore: add in chapter 02 demo02

---
+++ capitulo02/demo02-switch-case.js
@@ -0,0 +1,115 @@
+const readLine = require('readline')
+const terminal = readLine.createInterface({
+    // definir o modo de entrada via terminal
+    input: process.stdin,
+    // todo texto de saida sairá no terminal
+    output: process.stdout
+})
+
+// texto do menu
+const textoMenu = `
+Olá, seja bem vindo ao sistema de media
+Digite 1 para acessar o menu inicial
+Digite 2 para acessar o menu de Herois
+Digite 3 para acessar o menu de Guerreiras
+Digite 0 para sair
+`
+// console.log('textoMenu', textoMenu)
+// const opcao = 2
+// switch (opcao) {
+//     case 1:
+//         console.log('pressionou 1')
+//         break;
+//     case 2:
+//         console.log('pressionou 2')
+//         break;
+//     default:
+//         console.log('opção inválida')
+//         break
+// }
+
+// terminal.question('Qual é o seu nome?', (msg) => {
+//     console.log('Você escreveu', msg)
+//     terminal.close()
+// })
+
+const questoes = {
+    menuInicial: {
+        texto: textoMenu,
+        fn: menuInicial
+    },
+    opcao1: {
+        texto: 'Submenu! Pressione enter para selecionar mais opções...',
+        fn: opcao1
+    }
+}
+
+function opcao1(msg) {
+    console.log('Não há mais opções!')
+    terminal.close()
+}
+
+function menuInicial(msg) {
+    const opcao = Number(msg)
+    if (isNaN(opcao)) {
+        throw new Error('Não é um número', msg)
+    }
+    switch (opcao) {
+        case 0:
+            console.log('Saindo...')
+            terminal.close()
+            break;
+        case 1:
+            console.log('Menu inicial')
+            terminal.question(
+                questoes.opcao1.texto,
+                questoes.opcao1.fn
+            )
+            break;
+        default:
+            console.log('Opção invalida!')
+            terminal.close()
+            break;
+    }
+}
+
+terminal.question(
+    questoes.menuInicial.texto,
+    questoes.menuInicial.fn
+)
+
+
+
+// testes
+// switch (0) {
+//     default:
+//         console.log('Funciona sem case!');
+// }
+
+// switch (0) {
+//     case 0:
+//         console.log('Hello World');
+//     case 1:
+//         console.log('Segunda Opção');
+// }
+
+// switch (true) {
+//     case 1 > 2:
+//         console.log('1 > 2')
+//         break;
+//     case 1 === 1:
+//         console.log('1 === 1')
+//         break;
+//     default:
+//         console.log('nenhum deles!');
+//         break;
+// }
+
+// switch (true) {
+//     case 1 < 2:
+//         console.log('1 < 2')
+//     case 1 === 1:
+//         console.log('1 === 1')
+//     default:
+//         console.log('pode ter mais de uma opção')
+// }
